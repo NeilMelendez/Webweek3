9ca6cde9de092968eb2e23c7125f6d2f9a202884
NeilMelendez <91741631+NeilMelendez@users.noreply.github.com>
2022-10-20 14:58:24 +0700

    push

---
+++ Exercise_1,0_part2.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import MyComponent from "./MyComponent";
+
+const root = ReactDOM.creatRoot(document.getElementById("root"));
+root.render(<MyComponent />);
